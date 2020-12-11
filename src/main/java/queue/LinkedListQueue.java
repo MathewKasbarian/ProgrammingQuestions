cb0f7ea40d0b55799bcbbdc16833efa9b6ac4d0d
MathewKasbarian <71024080+MathewKasbarian@users.noreply.github.com>
2020-12-11 10:51:13 -0500

    Update LinkedListQueue.java

--- src/main/java/queue/LinkedListQueue.java
+++ src/main/java/queue/LinkedListQueue.java
@@ -1,3 +1,36 @@
 package queue;
 
+import java.util.LinkedList;
+
+
+public class LinkedListQueue<T> {
+    LinkedList<String> list = new LinkedList<String>(); 
+
+	public void enqueue(String string) {
+        list.add(string); 
+
+	}
+
+	public Object size() {
+		return list.size();
+	}
+
+	public boolean isEmpty() {
+        boolean a=false;
+        if(list.size()==0){
+            a=true;
+        }
+        return a;
+	}
+
+	public String dequeue() {
+        String a=list.getFirst();
+       
+        list.removeFirst();
+       
+       
+    
+        return  a;
+	}
+}
 // YOUR CODE GOES HERE
